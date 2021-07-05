e83992d1023adc8e66f82dbf2fdc78d9d9fce880
jeffkojobans5 <67514352+jeffkojobans5@users.noreply.github.com>
2021-07-05 16:38:53 +0100

    mails

--- fotcgh/volunteer.php
+++ fotcgh/volunteer.php
@@ -6,14 +6,14 @@ $message = "The following information was submitted from the form on your websit
 ";
 $message .= "First Name: ".$_REQUEST["name"]."\n\n";
 $message .= "Email address: ".$_REQUEST["email"]."\n\n";
-$message .= "Message: ".$_REQUEST["message"]."\n\n";
+$message .= "Message: ".$_REQUEST["phone"]."\n\n";
 $headers2 = "MIME-Version: 1.0\r\n"
            ."Content-Type: $contentType; charset=utf-8\r\n"
            ."Content-Transfer-Encoding: 8bit\r\n"
            ."From: =?UTF-8?B?".base64_encode(".$email.")."?= <".$email.">\r\n"
            ."Reply-To: ".$email."\r\n"
            ."X-Mailer: PHP/".phpversion();
-mail( $email, "Hi, Martin check this contact out", $message, $headers2 );
+mail( $email, "Hi, check this voluteer out ", $message,$headers2 );
 header("Location: http://fotcgh.org/thanks.html");
 ?>
 
